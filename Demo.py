4944e74566fbbac474a23a20d1ff8a890f669ece
Oh-mini <searom98@gmail.com>
2024-07-22 16:44:42 +0900

    first commit

---
+++ Demo.py
@@ -0,0 +1,16 @@
+# Demo.py
+
+print("Hello VS Code")
+
+#반복문
+#중단점(Break point)
+for i in [1,2,3]:
+    print(i)
+
+str = "python is powerful"
+print(str[0:5], type(str))
+
+colors=["red","green","yellow"]
+colors.append("blue")
+colors.insert(1, "purple")
+print(colors)
