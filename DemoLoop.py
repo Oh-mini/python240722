c08f247ddf49627135dbd352251c46a1773175ec
Oh-mini <searom98@gmail.com>
2024-07-23 09:24:27 +0900

    함수 연습

--- DemoLoop.py
+++ DemoLoop.py
@@ -8,3 +8,17 @@ while value > 0 :
 lst = ["apple", 100, 3.14]
 for item in lst:
     print(item)
+
+# range 함수
+print("---range() 함수---")
+print(list(range(1,11)))
+print(list(range(2000,2025)))
+print(list(range(1,32)))
+
+
+# List 내장
+print("---리스트 내장---")
+lst = list(range(1,11))
+print([i**2 for i in lst if i>5])
+tp = ("apple", "orange")
+print([len(i) for i in tp])
